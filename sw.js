ec0335606caf6e9d248c1291befa57e44849ab11
adaolopesvila6-oss <adaolopesvila6@gmail.com>
2026-03-30 23:28:29 +0100

    Criar sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+const C="cm-v3",F=["/","/index.html","/app.js"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(F)).then(()=>self.skipWaiting()))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(res&&res.status===200){const c=res.clone();caches.open(C).then(ca=>ca.put(e.request,c))}return res}).catch(()=>caches.match("/index.html"))))});
